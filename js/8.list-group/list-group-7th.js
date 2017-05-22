ecc5711290fd47ecde408fc46e59df1db73512bb
poohding <bowaxwing@gmail.com>
2017-05-22 14:32:35 +0900

    list-group-7th

--- js/8.list-group/list-group-7th.js
+++ js/8.list-group/list-group-7th.js
@@ -12,4 +12,85 @@
             element.dispatchEvent(event);
         }
     };
+
+    skpui.listGroup = (function() {
+        function ExampleList(options) {
+            this.lists = document.querySelectorAll(options.targetElement);
+            this.activeIndex = 0;
+            this.onChangeList = options.onChangeList;
+        }
+
+        ExampleList.prototype.init = function() {
+            this.addHandler();
+        };
+
+        ExampleList.prototype.showBadge = function(element) {
+            element.querySelector('.badge').classList.remove('hide');
+        };
+
+        ExampleList.prototype.hideBadge = function(element) {
+            element.querySelector('.badge').classList.add('hide');
+        };
+
+        ExampleList.prototype.activeList = function(activeIndex) {
+            this.activeIndex = activeIndex;
+
+            this.lists.forEach(function(element, index) {
+                if (activeIndex === index) {
+                    element.classList.add('active');
+                    skpui.util.trigger(element, 'list.active');
+                } else {
+                    element.classList.remove('active');
+                    skpui.util.trigger(element, 'list.deactive');
+                }
+            });
+
+            if (typeof this.onChangeList === 'function') {
+                this.onChangeList(this.activeIndex);
+            }
+        };
+
+        ExampleList.prototype.addHandler = function() {
+            var that = this;
+
+            this.lists.forEach(function(element, index) {
+                element.addEventListener('click', function(e) {
+                    e.preventDefault();
+
+                    that.activeList(index);
+                });
+
+                element.addEventListener('list.active', function() {
+                    that.showBadge(this);
+                });
+
+                element.addEventListener('list.deactive', function() {
+                    that.hideBadge(this);
+                });
+            });
+        };
+
+        document.addEventListener('DOMContentLoaded', function() {
+            var list1,
+                list2;
+
+            list1 = new ExampleList({
+                targetElement: '#listGroup1 .list-group-item',
+                onChangeList: function(index) {
+                    console.log(index);
+                }
+            });
+            list1.init();
+
+            list2 = new ExampleList({
+                targetElement: '#listGroup2 .list-group-item',
+                onChangeList: function(index) {
+                    console.log(index);
+                }
+            });
+            list2.init();
+        });
+
+        return ExampleList;
+    }());
 }(window.skpui = window.skpui || {}));
